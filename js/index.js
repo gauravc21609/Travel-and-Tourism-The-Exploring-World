926d37d49d9e95438c98bfba3c19b2acafd84047
gaurav <gchauhan21609@gmail.com>
2023-07-11 12:07:33 +0530

    only because i want passanger details and etc..

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
-document.getElementById("book-now").addEventListener("submit", function (e) {
-  e.preventDefault();
+// document.getElementById("book-now").addEventListener("submit", function (e) {
+//   e.preventDefault();
 
-  let destination = document.getElementById("destination").value;
-  let checkInDate = document.getElementById("check-in").value;
-  let type = document.getElementById("travel-type").value;
+//   let destination = document.getElementById("destination").value;
+//   let checkInDate = document.getElementById("check-in").value;
+//   let type = document.getElementById("travel-type").value;
 
-  bookDestination(destination, checkInDate, type);
-});
+//   bookDestination(destination, checkInDate, type);
+// });
 
-async function bookDestination(destination, checkInDate, travelType) {
-  let sendData = {
-    destination: destination,
-    checkInDate: checkInDate,
-    travelType: travelType,
-  };
+// async function bookDestination(destination, checkInDate, travelType) {
+//   let sendData = {
+//     destination: destination,
+//     checkInDate: checkInDate,
+//     travelType: travelType,
+//   };
 
-  let res = fetch("http://localhost:3000/booking", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(sendData),
-  });
+//   let res = fetch("http://localhost:3000/booking", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(sendData),
+//   });
 
-  let data = res.json()
-  console.log(data);
-}
+//   let data = res.json()
+//   console.log(data);
+// }
